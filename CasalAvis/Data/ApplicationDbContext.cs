cc92b84d0171e6e57d5aade3cf29c55141f59899
Erduma <ericduranmartinez2@gmail.com>
2022-11-28 21:13:31 +0100

    Agregar archivos de proyecto.

---
+++ CasalAvis/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasalAvis.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ CasalAvis/Data/ServeiDades.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CasalAvis.Data
+{
+    public class ServeiDades: DbContext
+    {
+        public string usuariAutenticat = "";
+
+        public readonly ApplicationDbContext dbContext;
+
+        public ServeiDades(ApplicationDbContext _db)
+        {
+            dbContext = _db;
+        }
+    }
+}
