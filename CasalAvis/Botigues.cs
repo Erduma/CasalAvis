812833d7848b5b40a4c9c3a5428c2b1abe27add3
Erduma <ericduranmartinez2@gmail.com>
2022-12-20 18:02:12 +0100

    mapa millora

---
+++ CasalAvis/Botigues.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasalAvis
+{
+    public class Botigues
+    {
+        [Key]
+        public int IdBotiga { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public string? NomBotiga { get; set; }
+    }
+}
